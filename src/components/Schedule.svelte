e554a05bf2667935c8129c9a3f1c4aa427e5e1dc
joren.sjongers <joren.sjongers@icloud.com>
2021-01-11 17:03:28 +0100

    show schedule on a grid

--- src/components/Schedule.svelte
+++ src/components/Schedule.svelte
@@ -1,19 +1,55 @@
 <script lang=typescript>
 
     import type { ScheduleData } from '../types/schedule-data';
+    import { ShiftType } from '../types/schedule-data';
+
 
     export let schedule_data: ScheduleData;
 
+    let days: number[] = Array.from({length: schedule_data.nb_days}, (_, i) => i + 1);
+
+    function getColor(assignment: ShiftType): string {
+        switch (assignment) {
+            case ShiftType.JA_E:
+                return "bg-yellow-400";
+            case ShiftType.JA_NW:
+                return "bg-green-400";
+            case ShiftType.JA_WH:
+                return "bg-red-500";
+            case ShiftType.SA_EW:
+                return "bg-green-500";
+            case ShiftType.SA_WH:
+                return "bg-red-600";
+            case ShiftType.C:
+                return "bg-green-600";
+            case ShiftType.TS:
+                return "bg-red-700";
+        }
+    }
 </script>
 
-
+<!-- TODO: Make first column sticky -->
 <main>
-    {#each schedule_data.individual_schedules as individual_schedule}
-        <p> {individual_schedule.employee_nb} </p>
-        <div>
-        {#each individual_schedule.assignments as assignment}
-            <div> {assignment} </div>
-        {/each}
+    <div class="flex-col">
+        <div class="sticky top-0 flex flex-row w-screen space-x-10 items-center bg-white">
+            <div class="flex flex-none w-24 h-16 bg-white"></div>
+            {#each days as day}
+                <div class="flex flex-none w-16 h-16 justify-center place-items-center bg-white">
+                    <p class="font-bold text-black"> day {day}</p>
+                </div>
+            {/each} 
+        </div>
+        {#each schedule_data.individual_schedules as individual_schedule}
+        <div class="mb-5 flex flex-row flex-none space-x-10 items-center">
+            <div class="sticky flex flex-none w-24 h-16 justify-center place-items-center">
+                <p class="font-bold text-black"> assistant { individual_schedule.employee_nb }</p>
+            </div>
+            {#each individual_schedule.assignments as assignment}
+                <div class="flex flex-none w-16 h-16 {getColor(assignment)} rounded-lg justify-center place-items-center">
+                    <p class="font-bold text-white"> {assignment}</p>
+                </div>
+            {/each}
         </div>
     {/each}
+    </div>
 </main>
