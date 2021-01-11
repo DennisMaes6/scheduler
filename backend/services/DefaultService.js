cf6ab15cd297bc8b977c9ccfaefc67f8e485e2f8
joren.sjongers <joren.sjongers@icloud.com>
2021-01-11 22:09:33 +0100

    implmenent schedule route

--- backend/services/DefaultService.js
+++ backend/services/DefaultService.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
 
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
 /**
 * Returns a generated schedule.
 *
@@ -9,8 +12,9 @@ const Service = require('./Service');
 const scheduleGET = () => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-      }));
+      resolve(Service.successResponse(
+        await getScheduleString()
+      ));
     } catch (e) {
       reject(Service.rejectResponse(
         e.message || 'Invalid input',
@@ -23,3 +27,33 @@ const scheduleGET = () => new Promise(
 module.exports = {
   scheduleGET,
 };
+
+
+async function getScheduleString() {
+  const scheduleString = await (await exec('minizinc --solver gurobi minizinc/scheduler.mzn minizinc/data.dzn')).stdout
+  const lines = scheduleString.split('\n')
+  const nb_days = lines[0].split(' ').slice(-2)[0].split(":")[1]
+  const shift_types = ["JA_E", "JA_NW", "JA_WH", "SA_EW", "SA_WH", "TS", "C", "F"]
+  const assistants = []
+  const individual_schedules = []
+  lines.slice(1, -2).forEach((line) => {
+    assistants.push({
+      id: line.split(' ')[0].split(':')[1],
+      type: line.split(' ')[1].split(':')[1]
+    })
+
+    const individual_schedule = {assistant_id: line.split(' ')[0].split(':')[1], assignments: []}
+    line.split(' ').slice(2, -1).forEach((shift)=>{
+      individual_schedule.assignments.push(shift)
+    })
+
+    individual_schedules.push(individual_schedule)
+  })
+
+  return {
+    nb_days,
+    assistants,
+    shift_types,
+    individual_schedules
+  }
+}
