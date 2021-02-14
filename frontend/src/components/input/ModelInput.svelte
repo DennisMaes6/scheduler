0ec0a34640f18f9381a7dee77781f4ad34ad5055
joren.sjongers <joren.sjongers@icloud.com>
2021-02-14 16:54:21 +0100

    change parameters model

--- frontend/src/components/input/ModelInput.svelte
+++ frontend/src/components/input/ModelInput.svelte
@@ -1,8 +1,27 @@
 <script lang=typescript>
     
-    import type { ModelParameters } from "../../openapi";
+    import { ModelParameters, ShiftType } from "../../openapi";
 
-    let modelParams: ModelParameters
+    let modelParams: ModelParameters = {
+        balance_minimum: 3,
+        balance_shifts: [ShiftType.JAEV, ShiftType.JANW, ShiftType.TSPT],
+        fairness_shift_weights: [
+            {
+                shift_type: ShiftType.JAEV,
+                weight: 0,
+            },
+            {
+                shift_type: ShiftType.JANW,
+                weight: 1,
+            },
+            {
+                shift_type: ShiftType.TSPT,
+                weight: 2
+            }
+        ]
+
+        
+    }
 
 </script>
 
