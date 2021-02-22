fecb423e02860746f13fbfb8780b7d227d6d2aec
joren.sjongers <joren.sjongers@icloud.com>
2021-02-22 21:47:34 +0100

    make most unfair shift types an array to model ties

--- backend/model/model_schedule.go
+++ backend/model/model_schedule.go
@@ -11,7 +11,7 @@ package model
 
 // Schedule - Holds all data of a generated schedule.
 type Schedule struct {
-	MostUnfairShift ShiftType `json:"most_unfair_shift,omitempty"`
+	MostUnfairShifts []ShiftType `json:"most_unfair_shifts,omitempty"`
 
 	// The fairness score of this schedule
 	FairnessScore float32 `json:"fairness_score,omitempty"`
