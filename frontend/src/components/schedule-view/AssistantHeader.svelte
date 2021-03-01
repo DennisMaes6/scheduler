8172eb66c4ef26bc937803672362fd32bee4ea13
joren.sjongers <joren.sjongers@icloud.com>
2021-03-01 09:11:10 +0100

    improve visuals

--- frontend/src/components/schedule-view/AssistantHeader.svelte
+++ frontend/src/components/schedule-view/AssistantHeader.svelte
@@ -3,13 +3,23 @@
     import type { Assistant } from '../../openapi'
     
     export let assistant: Assistant
+    export let max_workload: boolean
+    export let min_workload: boolean
 
+    function getWorkloadTextStyle() {
+        if (max_workload) {
+            return "text-red-500 font-bold"
+        } else if (min_workload) {
+            return "text-green-500 font-bold"
+        } else {
+            return "text-gray-700"
+        }
+    }
 </script>
 
 <main>
-    <div class="flex flex-row h-6 items-center justify-end cursor-default">
-        
-        <p class="mx-2 w-12 text-gray-700 text-xs font-bold"> {assistant.workload} </p>
-        <p class="mx-2 w-12 text-black text-xs font-bold"> {assistant.type} </p>
+    <div class="flex flex-row h-6 justify-between place-items-center cursor-default">
+        <p class="mx-2 {getWorkloadTextStyle()} text-xs"> {assistant.workload.toFixed(2)} </p>
+        <p class="mx-2 text-black text-xs font-bold"> {assistant.type} </p>
     </div>
 </main>
--- frontend/src/components/schedule-view/ScheduleView.svelte
+++ frontend/src/components/schedule-view/ScheduleView.svelte
@@ -10,22 +10,22 @@
 
     let days: number[] = Array.from({length: schedule.nb_days}, (_, i) => i + 1)
 
+    let max_workload = Math.max(...schedule.assistants.map((a) => a.workload))
+    let min_workload = Math.min(...schedule.assistants.map((a) => a.workload))
+
 </script>
 
 <main>
     <div class="ml-12 my-5 font-bold text-sm">
-        <p> fairness score: {schedule.fairness_score} </p>
+        <p> fairness score: {schedule.fairness_score.toFixed(2)} </p>
         <p> balance score: {schedule.balance_score} </p>
     </div>
-    <div class="pl-6 flex flex-row pr-2">
+    <div class="m-10 pl-6 flex flex-row pr-2">
         <!-- Assistant list -->
-        <div class="flex flex-col mx-4 space-y-2">
-            <div class="flex flex-row h-12">
-                <p class="mx-2 w-16 text-xs"> High workload </p>
-                <p class="mx-2 w-16 text-xs"> Low workload </p>
-            </div> 
+        <div class="flex flex-col w-32 mx-4 space-y-2">
+            <p class="mx-2 mt-4 text-xs"> Load </p>
             {#each schedule.assistants as assistant}
-                <AssistantHeader {assistant}/>
+                <AssistantHeader {assistant} max_workload={assistant.workload === max_workload} min_workload={assistant.workload === min_workload}/>
             {/each}
         </div>
         <div class="flex flex-col space-y-2 overflow-x-scroll">
