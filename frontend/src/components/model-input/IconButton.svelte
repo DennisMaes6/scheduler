7092389cc17560bdade9b7cd7d9aacb83368901b
joren.sjongers <joren.sjongers@icloud.com>
2021-03-19 15:24:39 +0100

    add holidays to frontend

---
+++ frontend/src/components/model-input/IconButton.svelte
@@ -0,0 +1,9 @@
+<script lang=typescript>
+
+    export let callback: () => void
+    
+</script>
+
+<button on:click={callback} type="button" class="focus:outline-none border-transparent">
+    <slot/>
+</button>
