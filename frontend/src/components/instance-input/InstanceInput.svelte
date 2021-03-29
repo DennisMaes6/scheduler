fa90e77d685452ddeddff9b0b2cc04e9df2ba296
joren.sjongers <joren.sjongers@icloud.com>
2021-03-29 22:48:20 +0200

    add assistant modal

--- frontend/src/components/instance-input/InstanceInput.svelte
+++ frontend/src/components/instance-input/InstanceInput.svelte
@@ -1,60 +1,27 @@
 <script lang=typescript>
     
-    import type { InstanceData, AssistantInstance} from "../../openapi";
-    import { AssistantType } from "../../openapi";
+    import type { InstanceData, AssistantInstance } from "../../openapi";
 
+    import { AssistantType } from "../../openapi";
     import { Service } from '../../openapi';
 
+    import Modal from "../Modal.svelte";
     import Button from "../model-input/Button.svelte";
     import IconButton from "../model-input/IconButton.svelte";
     import InputField from "../model-input/InputField.svelte";
 
     export let data: InstanceData
 
-    function count(type: AssistantType): number {
-        return data.assistants.filter((ai) => ai.type === type).length
-    }
-
-    let counts = [
-        {
-            type: AssistantType.JA,
-            count: count(AssistantType.JA),
-        },
-        {
-            type: AssistantType.JA_F,
-            count: count(AssistantType.JA_F),
-        },
-        {
-            type: AssistantType.SA,
-            count: count(AssistantType.SA),
-        },
-        {
-            type: AssistantType.SA_F,
-            count: count(AssistantType.SA_F),
-        },
-        {
-            type: AssistantType.SA_NEO,
-            count: count(AssistantType.SA_NEO),
-        },
-        {
-            type: AssistantType.SA_F_NEO,
-            count: count(AssistantType.SA_F_NEO),
-        },
+    const types: AssistantType[] = [
+        AssistantType.JA,
+        AssistantType.JA_F,
+        AssistantType.SA,
+        AssistantType.SA_F,
+        AssistantType.SA_NEO,
+        AssistantType.SA_F_NEO
     ]
 
     function handleSubmit() {
-        let index = 0
-        let newData: AssistantInstance[] = [] 
-        counts.forEach((countInstance) => {
-            for (let j = 0; j < countInstance.count; j++) {
-                index++
-                newData.push({
-                    id: index,
-                    type: countInstance.type
-                })
-            }
-        })
-        data.assistants = newData
         Service.postInstanceData(data)
     }
     
@@ -66,6 +33,31 @@
     function removeHoliday(i: number) {
         data.holidays.splice(i, 1)
         data.holidays = data.holidays
+    }
+
+    function removeAssistant(assistant: AssistantInstance) {
+        data.assistants = data.assistants.filter((a) => a.id != assistant.id)
+        let index = 1
+        data.assistants.forEach((a) => {
+            a.id = index
+            index++
+        })
+    }
+
+    function addAssistant(type: AssistantType) {
+        data.assistants.push(
+            {
+                id: Math.max(...data.assistants.map((a) => a.id)) + 1,
+                type,
+                free_days: []
+            }
+        )
+        let index = 1
+        data.assistants.forEach((a) => {
+            a.id = index
+            index++
+        })
+        data.assistants = data.assistants
     }
 
 </script>
@@ -87,22 +79,49 @@
         {/each}
         </div>
         <IconButton callback={addHoliday}><span class="h-6 w-6 color-gray-500 iconify" data-icon="ic:baseline-add-circle-outline" data-inline="false"></span></IconButton>
-
-        <p class="mt-1 font-semibold text-xs text-gray-500 cursor-default"> Number of assistants </p>
-        <div class="mt-1 flex flex-col">
-            {#each counts as countInstance}
-                <div class="flex flex-row space-x-1 justify-between">
-                    <p class="mx-2 w-12 text-black text-xs font-bold"> {countInstance.type} </p>
-                    <div class=w-12>
-                        <InputField bind:value={countInstance.count}/>
-                    </div>
+        <Modal>
+            <div slot="trigger" let:open>
+                <Button callback={open}> Edit assistants </Button>
+            </div>
+            <div class="my-2" slot="header">
+                <h1 class="font-bold"> Edit assistants </h1>
+            </div>
+            <div class="my-2" slot="content">
+                <div class="flex flex-col space-y-2">
+                    {#each types as type}
+                        <h1 class="my-2"> {type} </h1>
+                        {#each data.assistants.filter((a) => a.type == type) as assistant (assistant.id)}
+                            <div class="flex flex-row space-x-2">
+                                <p class="w-16 text-black text-sm"> {assistant.type} </p>
+                                <Modal>
+                                    <div slot="trigger" let:open>
+                                        <Button callback={open}> Edit </Button>
+                                    </div>
+                                    <div class="my-2" slot="content">
+                                        test
+                                    </div>
+                                </Modal>
+                                
+                                <IconButton callback={() => removeAssistant(assistant)}>
+                                    <span class="h-6 w-6 color-gray-500 iconify" 
+                                          data-icon="ic:round-remove-circle-outline" 
+                                          data-inline="false"/>
+                                </IconButton>
+                            </div>
+                        {/each}
+                        <Button callback={() => addAssistant(type)}> New </Button>
+                    {/each}
+  
                 </div>
-            {/each}
-        </div>
+            </div>
+            <div class="my-5 flex flex-row justify-end space-x-2" slot="footer" let:close>
+                <Button primary={false} callback={close}> Close </Button>
+                <Button callback={handleSubmit}> Submit </Button>
+            </div>
 
+        </Modal>
         <div class="my-1 mx-auto">
             <Button callback={handleSubmit}> Submit </Button>
         </div>
     </form>
-
 </main>
--- frontend/src/components/model-input/Button.svelte
+++ frontend/src/components/model-input/Button.svelte
@@ -2,9 +2,16 @@
 <script lang=typescript>
 
     export let callback: () => void
+    export let primary: boolean = true
     
 </script>
 
-<button on:click={callback} type="button" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded text-xs focus:outline-none border-transparent">
-    <slot/>
-</button>
+{#if primary}
+    <button on:click={callback} type="button" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded text-xs focus:outline-none border-transparent">
+        <slot/>
+    </button>
+{:else}
+    <button on:click={callback} type="button" class="hover:bg-gray-100 text-green-500 font-bold py-1 px-2 rounded text-xs focus:outline-none border-transparent">
+        <slot/>
+    </button>
+{/if}
