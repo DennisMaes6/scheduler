294c09da1c5eb9adfac93bac7cd2f42889f02998
joren.sjongers <joren.sjongers@icloud.com>
2021-04-09 11:38:04 +0200

    add sorting functionality

---
+++ backend/schedule_generator/sort.go
@@ -0,0 +1,51 @@
+package schedule_generator
+
+import "github.com/jorensjongers/scheduler/backend/model"
+
+// Sort interface methods for assistants
+type ById []model.Assistant
+
+func (a ById) Len() int {
+	return len(a)
+}
+
+func (a ById) Less(i, j int) bool {
+	return a[i].Id < a[j].Id
+}
+
+func (a ById) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+// Sort interface methods for shift type parameters
+type ByShiftType []model.ShiftTypeModelParameters
+
+var shiftTypeOrder []model.ShiftType = []model.ShiftType{
+	model.JANW,
+	model.JAWH,
+	model.SAEW,
+	model.SAWH,
+	model.TSPT,
+	model.CALL,
+}
+
+func (a ByShiftType) Len() int {
+	return len(a)
+}
+
+func (a ByShiftType) Less(i, j int) bool {
+	return index(shiftTypeOrder, a[i].ShiftType) < index(shiftTypeOrder, a[j].ShiftType)
+}
+
+func (a ByShiftType) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func index(slice []model.ShiftType, item model.ShiftType) int {
+	for i, _ := range slice {
+		if slice[i] == item {
+			return i
+		}
+	}
+	return -1
+}
