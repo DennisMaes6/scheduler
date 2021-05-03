ff007f9a4d9d9ee921a5ee50261f8513f93737dc
joren.sjongers <joren.sjongers@icloud.com>
2021-05-03 14:17:46 +0200

    refactor schedule view

--- frontend/src/components/schedule-view/ScheduleView.svelte
+++ frontend/src/components/schedule-view/ScheduleView.svelte
@@ -1,6 +1,6 @@
 <script lang=typescript>
 
-    import type { Schedule, InstanceData, Assignment } from '../../openapi';
+    import type { Schedule, InstanceData, Assignment, Assistant } from '../../openapi';
     
     import AssistantHeader from './AssistantHeader.svelte'
     import DayHeader from './DayHeader.svelte';
@@ -12,8 +12,8 @@
     let max_workload = Math.max(...schedule.individual_schedules.map((a) => a.workload))
     let min_workload = Math.min(...schedule.individual_schedules.map((a) => a.workload))
 
-    function get_workload(id: number): number {
-       return schedule.individual_schedules.find((s) => s.assistant_id == id).workload
+    function getAssistant(id: number): Assistant {
+        return data.assistants.find(a => a.id === id);
     }
 
     function get_assignment(assistant_id: number, day_id: number): Assignment {
@@ -33,8 +33,8 @@
         <!-- Assistant list -->
         <div class="flex flex-col w-32 mx-4 space-y-2">
             <p class="mt-4 text-xs"> Workload </p>
-            {#each data.assistants as assistant}
-                <AssistantHeader {assistant} workload={get_workload(assistant.id)} {max_workload} {min_workload}/>
+            {#each schedule.individual_schedules as is}
+                <AssistantHeader assistant={getAssistant(is.assistant_id)} workload={is.workload} {max_workload} {min_workload}/>
             {/each}
         </div>
         <div class="flex flex-col space-y-2 overflow-x-scroll">
@@ -45,10 +45,10 @@
                 {/each}
             </div>
             <!-- Schedule -->
-            {#each data.assistants as assistant}
+            {#each schedule.individual_schedules as is}
                 <div class="flex flex-row space-x-2">
                     {#each data.days as day}
-                        <AssignmentBox assignment={get_assignment(assistant.id, day.id)}/>
+                        <AssignmentBox assignment={is.assignments.find(a => a.day_nb === day.id)}/>
                     {/each}
                 </div>
             {/each}
