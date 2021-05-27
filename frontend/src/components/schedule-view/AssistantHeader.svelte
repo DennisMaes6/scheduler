bcdbf2e22d2b02725ecfc005c48ea5137fa76890
joren.sjongers <joren.sjongers@icloud.com>
2021-05-27 21:50:31 +0200

    order assistants by type on schedule view

--- frontend/src/components/schedule-view/AssistantHeader.svelte
+++ frontend/src/components/schedule-view/AssistantHeader.svelte
@@ -1,6 +1,7 @@
 <script lang=typescript>
 
     import type { Assistant } from '../../openapi'
+    import { AssistantType } from '../../openapi'
     
     export let assistant: Assistant
     export let workload: number
@@ -20,6 +21,19 @@
 
 
 <div class="flex flex-row flex-none h-6 justify-between place-items-center cursor-default">
+    {#if assistant.type === AssistantType.JA}
+        <p class="text-xs mr-2">JA</p>
+    {:else if assistant.type === AssistantType.JA_F}
+        <p class="text-xs mr-2">JA<sub>F</sub></p>
+    {:else if assistant.type === AssistantType.SA}
+        <p class="text-xs mr-2">SA</p>
+    {:else if assistant.type === AssistantType.SA_F}
+        <p class="text-xs mr-2">SA<sub>F</sub></p>
+    {:else if assistant.type === AssistantType.SA_NEO}
+        <p class="text-xs mr-2">SA<sub>N</sub></p>
+    {:else if assistant.type === AssistantType.SA_F_NEO}
+        <p class="text-xs mr-2">SA<sub>FN</sub></p>
+    {/if}
     <p class="{getWorkloadTextStyle()} text-xs"> {workload.toFixed(2)} </p>
     <p class="w-32 text-right text-black text-xs font-bold "> {assistant.name} </p>
 </div>
--- frontend/src/components/schedule-view/ScheduleView.svelte
+++ frontend/src/components/schedule-view/ScheduleView.svelte
@@ -1,6 +1,7 @@
 <script lang=typescript>
 
-    import type { Schedule, InstanceData, Assistant } from '../../openapi';
+    import type { Schedule, InstanceData, Assistant, IndividualSchedule } from '../../openapi';
+    import { AssistantType } from '../../openapi';
     
     import AssistantHeader from './AssistantHeader.svelte'
     import DayHeader from './DayHeader.svelte';
@@ -17,6 +18,12 @@
     function getAssistant(id: number): Assistant {
         return data.assistants.find(a => a.id === id);
     }
+
+    function getIs(id: number): IndividualSchedule {
+        return schedule.individual_schedules.find(s => s.assistant_id === id);
+    }
+
+    const types: AssistantType[] = Object.values(AssistantType)
 
     onMount(async () => {
 		let isSyncingLeftScroll: boolean = false;
@@ -66,8 +73,10 @@
         <div class="flex flex-none h-10"/> <!--placeholder-->
         <div id="assistantlist" class=" scrollbar-hidden flex flex-row overflow-y-scroll">
             <div  class="flex flex-none flex-col h-full space-y-1">
-                {#each schedule.individual_schedules as is}
-                    <AssistantHeader assistant={getAssistant(is.assistant_id)} workload={is.workload} {max_workload} {min_workload}/>
+                {#each types as type}
+                    {#each data.assistants.filter((a) => a.type == type) as assistant (assistant.id)}
+                        <AssistantHeader {assistant} workload={getIs(assistant.id).workload} {max_workload} {min_workload}/>
+                    {/each}
                 {/each}
             </div>
         </div>
@@ -82,15 +91,17 @@
             {/each}
         </div>
         <div id="schedule" class="scrollable flex flex-col h-full w-full overflow-scroll space-y-1">
-            {#each schedule.individual_schedules as is}
-                <div class="flex flex-row w-full space-x-1">
-                    {#each data.days as day}
-                        <AssignmentBox assignment={is.assignments.find(a => a.day_nb === day.id)} free_day={getAssistant(is.assistant_id).free_days.includes(day.id)}/>
-                        {#if day.id % 7 === 0}
-                            <div class="flex flex-none w-4"/>
-                        {/if}
-                    {/each}
-                </div>
+            {#each types as type}
+                {#each data.assistants.filter((a) => a.type == type) as assistant (assistant.id)}
+                    <div class="flex flex-row w-full space-x-1">
+                        {#each data.days as day}
+                            <AssignmentBox assignment={getIs(assistant.id).assignments.find(a => a.day_nb === day.id)} free_day={assistant.free_days.includes(day.id)}/>
+                             {#if day.id % 7 === 0}
+                                <div class="flex flex-none w-4"/>
+                            {/if}
+                        {/each}
+                    </div>
+                {/each}
             {/each}
         </div>
     </div>
